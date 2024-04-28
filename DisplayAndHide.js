9e2fcad43260805e5cf4288299b5c0875b9a10ef
ayat-allah <166665397+ayat-allah@users.noreply.github.com>
2024-04-28 18:14:55 +0300

    Update DisplayAndHide.js

--- DisplayAndHide.js
+++ DisplayAndHide.js
@@ -1,7 +1,12 @@
+   //list for the text divs
    const textNodeList = document.querySelectorAll(".small");
+   //list for the two images in a row divs
    const twoImagesNodeList = document.querySelectorAll("div.one");
+   //list for the div of the images that are displayed three in a row
    const threeImagesNodeList = document.querySelectorAll("div.four");
+   //list for the div of the div that contains the div of the text and the div of the image in the three images in a row rows
    const threeImagesBoxNodeList = document.querySelectorAll("div.five");
+   //displaying 2 images by returning the index of the  div of image and the div of the text
    function displayTwo(index2, index1){
    if(index1%5 == 1){
     twoImagesNodeList[index2].style.order = "-1";
@@ -14,6 +19,7 @@
    textNodeList[index1].style.overflow = "auto";
    twoImagesNodeList[index2].style.cursor = "context-menu";
    }
+     //displaying 3 images by returning the index of the  div of image and the div of the text
    function displayThree(index2, index1){
    threeImagesBoxNodeList[index2].style.order = "-1";
    if(index1%5 == 3){
@@ -37,6 +43,7 @@
    textNodeList[index1].style.overflow = "auto";
    threeImagesNodeList[index2].style.cursor = "context-menu";
    }
+     //hiding 2 images by returning the index of the  div of image and the div of the text
    function hideTwo(ind1, ind2){
    if(ind1%5 == 1){
     twoImagesNodeList[ind2].style.order = "1";
@@ -44,6 +51,7 @@
    textNodeList[ind1].style.display = "none";
    twoImagesNodeList[ind2].style.cursor = "pointer";
    }
+     //hiding 3 images by returning the index of the div of image and the div of the text
    function hideThree(ind1, ind2){
    threeImagesBoxNodeList[ind2].style.flex = "0 0 32.9%";
    if(ind1%5 == 3){
