091be119225aa717fffb8af4761648fe78635683
Jana <155358756+janaalkot@users.noreply.github.com>
2024-04-30 23:49:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-function changeStyle() {
-var currentStyle= document.getElementById('stylesheet').getAttribute('href');
-if(currentStyle==='mystyle.css'){
-document.getElementById('stylesheet').setAttribute('href','theme2.css');
-}
-else{
-    document.getElementById('stylesheet').setAttribute('href','mystyle.css');
-}
-}
-/*function changeStyle(){
-var button = document.getElementById("bmj");
-if(button.classList.contains("mystyle")){
-    button.classList.remove("mystyle.css");
-    button.classList.add("theme2.css");
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll('a[href^="#"]');
 
-}else{
-    button.classList.remove("theme2.css");
-    button.classList.add("mystyle.css");
-}
-
-}*/
+  links.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+           block: "start"
+        });
+      }
+    });
+  });
+});
