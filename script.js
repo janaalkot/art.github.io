cbe7c6afc74f0559abb2fcacfa0ffb729a4fa13c
ayat-allah <166665397+ayat-allah@users.noreply.github.com>
2024-05-01 21:39:04 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function changeStyle(){
-var currentStyle= document.getElementById('stylesheet').getAttribute('href');
-if(currentStyle==='ali.css'){
-document.getElementById('stylesheet').setAttribute('href','wafaa.css');
-}
-else{
-    document.getElementById('stylesheet').setAttribute('href','ali.css');
-}
